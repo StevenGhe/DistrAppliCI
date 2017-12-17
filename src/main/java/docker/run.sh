4fae8196c12b6cbf5fdc20b5b9393b58d50e3102
StevenGhe <stevenghekiere@hotmail.com>
2017-12-17 15:45:06 +0100

    docker & run

---
+++ src/main/java/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "--------------------------------"
+echo "Starting Destination Service Server"
+echo "--------------------------------"
+java -jar /usr/local/licensingservice/@project.build.finalName@.jar
